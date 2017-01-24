8a79a0778ff21568b792a8a561e5e257f0ea23a8
Graham Seaton <grahamseaton@grahams-mbp>
2017-01-24 11:32:20 +0000

    Initial Commit

---
+++ myCBT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myCBT
+//
+//  Created by Graham Seaton on 24/01/2017.
+//  Copyright © 2017 Graham Seaton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
